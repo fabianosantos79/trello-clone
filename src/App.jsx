83ccce2320f37afaed7c045143a857c14fb13e80
Fabiano <fabiano.ssantos79@gmail.com>
2023-04-16 04:15:54 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 
 const initialItems = [
-  { id: '1', content: 'Conteúdo do card 1' },
-  { id: '2', content: 'Conteúdo do card 2' },
-  { id: '3', content: 'Conteúdo do card 3' }
+  { id: '1', content: 'Desenhar o layout' },
+  { id: '2', content: 'Codificar o sistema' },
+  { id: '3', content: 'Testar e Publicar' }
 ]
 
 const initialColumns = [
@@ -22,16 +23,33 @@ function App() {
 
   return (
     <div className="App">
-      {initialColumns.map((column) => (
-        <div>
-          <h1>{column.name}</h1>
-          <div style={{ backgroundColor: 'lightblue', width: 250, height: 500 }}>
-            {column.items.map((item) => (
-              <div style={{ backgroundColor: '#dddddd', width: "30" }}></div>
-            ))}
-          </div>
-        </div>
-      ))}
+      <DragDropContext>
+        {initialColumns.map((column) => (
+          <Droppable droppableId={column.id}>
+            {(provided) => (
+              <div ref={provided.innerRef}>
+
+                <h1>{column.name}</h1>
+                <div style={{ backgroundColor: 'lightblue', width: 250, height: 500, padding: 10 }}>
+                  {column.items.map((item, index) => (
+                    <Draggable draggableId={item.id} index={index}>
+                      {(provided) => (
+                        <div
+                          {...provided.dragHandleProps}
+                          {...provided.draggableProps}
+                          ref={provided.innerRef}
+                          style={{ backgroundColor: 'gray', color: '#fff', height: 55, marginBottom: 10, ...provided.dragHandleProps.style }}>{item.content}
+                        </div>
+                      )}
+                    </Draggable>
+                  ))
+                  }
+                </div>
+              </div>
+            )}
+          </Droppable>
+        ))}
+      </DragDropContext>
     </div>
   )
 }
